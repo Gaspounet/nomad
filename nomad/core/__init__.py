a35a6d4839102231d46c333b0f846949623d286d
Gaspard <gaspounet@gmail.com>
2023-08-29 21:25:16 -0400

    Adding files to create a custom model for exec nodes

--- nomad/core/__init__.py
+++ nomad/core/__init__.py
@@ -8,9 +8,14 @@ Docstring with information
 # Python Core Import
 
 # Third-party Imports
+from NodeGraphQt.base.model import NodeModel
 
 # Nomad Imports
 
 
 if __name__ == "__main__":
+    pass
+
+
+class ExecNodeModel(NodeModel):
     pass
